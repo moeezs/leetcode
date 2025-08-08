8a02e5285c1631117b970686063480a58a3391cc
Abdul Moeez Shaikh <abdulmoeezshaikh@gmail.com>
2025-08-08 19:04:03 -0400

    Create 2022-Convert1DArrayInto2DArray

---
+++ 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.py
@@ -0,0 +1,35 @@
+# Last updated: 8/8/2025, 7:04:03 PM
+class Solution(object):
+    def construct2DArray(self, original, m, n):
+        """
+        :type original: List[int]
+        :type m: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        check_help = float(len(original) / float(m))
+        if (check_help / float(n)) != 1:
+            return []
+        
+        i = 0
+        n_help = 0
+        result = []
+        temp = []
+
+        while i < len(original):
+            if n_help < n:
+                temp.append(original[i])
+                n_help += 1
+            elif n_help == n:
+                result.append(temp)
+                temp = [original[i]]
+                n_help = 1
+            i += 1
+        
+        if temp:
+            result.append(temp)
+
+
+        return result
+
+        
