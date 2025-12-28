7047cd353e715032156546cb2b4741203422b7bb
Abdul Moeez Shaikh <abdulmoeezshaikh@gmail.com>
2025-12-28 18:31:14 -0500

    Create 1127-LastStoneWeight

---
+++ 1127-LastStoneWeight/1127-LastStoneWeight.py
@@ -0,0 +1,24 @@
+# Last updated: 12/28/2025, 6:31:14 PM
+import heapq
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        stones = [-x for x in stones]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            first = -heapq.heappop(stones)
+            second = -heapq.heappop(stones)
+            if first != second:
+                heapq.heappush(stones, -(first - second))
+            
+        if stones:
+            return -stones[0]
+        return 0
+                
+
+
+
+        
